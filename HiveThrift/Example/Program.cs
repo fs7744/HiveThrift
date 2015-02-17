a7af65644a3402b76c16dca4e1354cea52d114ca
Victor.X.Qu <fs7744@hotmail.com>
2015-02-17 23:06:01 +0800

    ADD simple hiveclient for hive server2

--- HiveThrift/Example/Program.cs
+++ HiveThrift/Example/Program.cs
@@ -1,63 +1,27 @@
 ﻿using Hive2;
 using System;
-using Thrift.Protocol;
-using Thrift.Transport;
 
 namespace Example
 {
-    public class HiveClient
+    internal class Program
     {
-        private TSaslClientTransport transport;
-        public TCLIService.Client client;
-        private TBinaryProtocol protocol;
-
-        public HiveClient(string host, int port, string userName = "None", string password = "None")
+        private static void Main(string[] args)
         {
-            var socket = new TSocket(host, port);
-            transport = new TSaslClientTransport(socket, userName, password);
-            protocol = new TBinaryProtocol(transport);
-            client = new TCLIService.Client(protocol);
+            var client = new HiveClient("10.1.54.42", 10000);
+            client.Open();
+            //Execute(client,"show databases");
+            Execute(client, "use lz33");
+            Execute(client, "show tables");
+            client.Close();
+            Console.ReadLine();
         }
 
-        public void Open()
+        public static void Execute(HiveClient client, string query)
         {
-            if (transport != null && !transport.IsOpen)
-                transport.Open();
-        }
-
-        public object Execute(string query)
-        {
-            object result = null;
             try
             {
-                TOpenSessionReq openReq = new TOpenSessionReq();
-                TOpenSessionResp openResp = client.OpenSession(openReq);
-                CheckStatus(openResp.Status);
-
-                TSessionHandle sessHandle = openResp.SessionHandle;
-
-                openReq = new TOpenSessionReq();
-                openResp = client.OpenSession(openReq);
-                CheckStatus(openResp.Status);
-
-                sessHandle = openResp.SessionHandle;
-
-                TExecuteStatementReq execReq = new TExecuteStatementReq();
-                execReq.SessionHandle = sessHandle;
-                execReq.Statement = query;
-                TExecuteStatementResp execResp = client.ExecuteStatement(execReq);
-                CheckStatus(execResp.Status);
-
-                TOperationHandle stmtHandle = execResp.OperationHandle;
-                TFetchResultsReq fetchReq = new TFetchResultsReq();
-                fetchReq.MaxRows = int.MaxValue;
-                fetchReq.OperationHandle = stmtHandle;
-                fetchReq.Orientation = TFetchOrientation.FETCH_NEXT;
-                TFetchResultsResp resultsResp = client.FetchResults(fetchReq);
-                CheckStatus(resultsResp.Status);
-
-                TRowSet resultsSet = resultsResp.Results;
-                result = resultsSet;
+                var operation = client.ExecuteStatement(query);
+                var resultsSet = client.FetchResult(operation);
                 foreach (var item in resultsSet.Columns)
                 {
                     if (item.StringVal.Values != null && item.StringVal.Values.Count > 0)
@@ -66,59 +30,12 @@ namespace Example
                             Console.WriteLine(info);
                         }
                 }
-
-                //TGetResultSetMetadataReq schemasReq = new TGetResultSetMetadataReq();
-                //schemasReq.OperationHandle = stmtHandle;
-                //TGetResultSetMetadataResp schemasResp = client.GetResultSetMetadata(schemasReq);
-                //CheckStatus(schemasResp.Status);
-                //result.Schemas = schemasResp.Schema.Columns;
-
-                TCloseOperationReq closeReq = new TCloseOperationReq();
-                closeReq.OperationHandle = stmtHandle;
-                TCloseOperationResp closeOperationResp = client.CloseOperation(closeReq);
-                CheckStatus(closeOperationResp.Status);
-
-                TCloseSessionReq closeSessionReq = new TCloseSessionReq();
-                closeSessionReq.SessionHandle = sessHandle;
-                TCloseSessionResp closeSessionResp = client.CloseSession(closeSessionReq);
-                CheckStatus(closeSessionResp.Status);
+                client.CloseOperation(operation);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-
-            return result;
-        }
-
-        public static void CheckStatus(TStatus status)
-        {
-            if ((TStatusCode)status.ErrorCode == TStatusCode.ERROR_STATUS || (TStatusCode)status.ErrorCode == TStatusCode.INVALID_HANDLE_STATUS)
-            {
-                throw new Exception(status.ErrorMessage);
-            }
-        }
-
-        public void Close()
-        {
-            if (transport != null && transport.IsOpen)
-                transport.Close();
-        }
-
-        public static string Way { get { return "Hive2"; } }
-    }
-
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var client = new HiveClient("10.1.54.42", 10000);
-            client.Open();
-            //client.Execute("show databases");
-            client.Execute("use lz33");
-            client.Execute("show tables");
-            client.Close();
-            Console.ReadLine();
         }
     }
 }
---
+++ HiveThrift/HiveThrift/Hive2/HiveClient.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace Hive2
+{
+    public class HiveClient
+    {
+        private TSaslClientTransport transport;
+        public TCLIService.Client client;
+        private TBinaryProtocol protocol;
+        private TSessionHandle m_Session;
+
+        public HiveClient(string host, int port, string userName = "None", string password = "None")
+        {
+            var socket = new TSocket(host, port);
+            transport = new TSaslClientTransport(socket, userName, password);
+            protocol = new TBinaryProtocol(transport);
+            client = new TCLIService.Client(protocol);
+        }
+
+        public void Open()
+        {
+            if (transport != null && !transport.IsOpen)
+                transport.Open();
+            if (m_Session == null)
+                m_Session = GetSession();
+        }
+
+        public void Close()
+        {
+            if (m_Session != null)
+                CloseSession();
+            if (transport != null && transport.IsOpen)
+                transport.Close();
+        }
+
+        private void CloseSession()
+        {
+            TCloseSessionReq closeSessionReq = new TCloseSessionReq();
+            closeSessionReq.SessionHandle = m_Session;
+            TCloseSessionResp closeSessionResp = client.CloseSession(closeSessionReq);
+            CheckStatus(closeSessionResp.Status);
+        }
+
+        public static void CheckStatus(TStatus status)
+        {
+            if ((TStatusCode)status.ErrorCode == TStatusCode.ERROR_STATUS || (TStatusCode)status.ErrorCode == TStatusCode.INVALID_HANDLE_STATUS)
+            {
+                throw new Exception(status.ErrorMessage);
+            }
+        }
+
+        private TSessionHandle GetSession()
+        {
+            TOpenSessionReq openReq = new TOpenSessionReq();
+            TOpenSessionResp openResp = client.OpenSession(openReq);
+            CheckStatus(openResp.Status);
+            return openResp.SessionHandle;
+        }
+
+        public TOperationHandle ExecuteStatement(string statement)
+        {
+            TExecuteStatementReq execReq = new TExecuteStatementReq();
+            execReq.SessionHandle = m_Session;
+            execReq.Statement = statement;
+            TExecuteStatementResp execResp = client.ExecuteStatement(execReq);
+            CheckStatus(execResp.Status);
+            return execResp.OperationHandle;
+        }
+
+        public void ExecuteStatementNoResult(string statement)
+        {
+            var operation = ExecuteStatement(statement);
+            CloseOperation(operation);
+        }
+
+        public TRowSet FetchResult(TOperationHandle operationHandle, TFetchResultsReq req = null)
+        {
+            if (req == null)
+            {
+                req = new TFetchResultsReq();
+                req.MaxRows = int.MaxValue;
+                req.Orientation = TFetchOrientation.FETCH_NEXT;
+            }
+            req.OperationHandle = operationHandle;
+            TFetchResultsResp resultsResp = client.FetchResults(req);
+            CheckStatus(resultsResp.Status);
+            return resultsResp.Results;
+        }
+
+        public void CloseOperation(TOperationHandle operationHandle)
+        {
+            TCloseOperationReq closeReq = new TCloseOperationReq();
+            closeReq.OperationHandle = operationHandle;
+            TCloseOperationResp closeOperationResp = client.CloseOperation(closeReq);
+            CheckStatus(closeOperationResp.Status);
+        }
+    }
+}
